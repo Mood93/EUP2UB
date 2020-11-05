a0abd883772eb05290be5d9d0799578c5ac6782a
Mood93 <mood93@hotmail.com>
2020-11-05 09:18:18 -0600

    started repo over, added EUP2UB.py

---
+++ EUP2UB.py
@@ -0,0 +1,45 @@
+# file handling
+fname = input("Enter file name: ")
+if len(fname) < 1 : fname = "wardrobe.ini"
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+
+#vars
+wardrobeName = ""
+wardrobeDict = dict()
+allWardrobes = dict()
+
+# xref of input keys to output keys
+xref = dict()
+xref = {
+"Hat": (0, "prop_hats", "tex_hats"),
+"Glasses": (1, "prop_glasses", "tex_glasses"),
+"Ear": (2, "prop_ears", "tex_ears"),
+"Watches": (3, "prop_watches", "tex_watches"),
+"Top": (4, "comp_shirt", "tex_shirt")
+}
+
+# consume wardrobe.ini
+for line in fhand:
+    line = line.rstrip()
+    if line.startswith("[") : #find start of individual wardrobe and save/print name
+        wardrobeName = line
+        #print(wardrobeName)
+    
+    if not line.startswith("[") : #for each prop, process into dict
+        propValues = line.split("=")
+        #print(propValues)
+        if propValues[0] in xref :
+            #print(propValues[1])
+            numbersList = propValues[1].split(":")
+            wardrobeDict[propValues[0]] = (numbersList[0], numbersList[1])
+            #print(numbersList)
+            #print(wardrobeDict)
+    
+    allWardrobes[wardrobeName] = wardrobeDict
+
+print(allWardrobes)
+#use dict.get to pull xref and create output 
