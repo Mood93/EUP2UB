5b8fc4ba24f239194da43cca9e1c04b4373935b4
Mood93 <mood93@hotmail.com>
2020-11-07 12:00:27 -0600

    add logic to include gender aware pedName

--- EUP2UB.py
+++ EUP2UB.py
@@ -1,6 +1,7 @@
 # file handling
-fname = input("Enter file name: ")
-if len(fname) < 1 : fname = "wardrobe.ini"
+#fname = input("Enter file name: ")
+#if len(fname) < 1 : fname = "wardrobe.ini"
+fname = "wardrobe.ini"
 try:
     fhand = open(fname)
 except:
@@ -11,6 +12,7 @@ except:
 wardrobeName = ""
 wardrobeDict = dict()
 allWardrobes = dict()
+pedName = ""
 
 # xref of input keys to output keys
 xref = dict()
@@ -40,30 +42,43 @@ for line in fhand:
     if not line.startswith("[") : #for each comp, process into dict
         compValues = line.split("=")
         #print(compValues)
+
+        if compValues[0] == "Gender" :
+            if compValues[1] == "Male" : wardrobeDict[compValues[0]] = ("Male",)
+            elif compValues[1] == "Female" : wardrobeDict[compValues[0]] = ("Female",)
+
         if compValues[0] in xref :
             #print(compValues[1])
             numbersList = compValues[1].split(":")
+
             wardrobeDict[compValues[0]] = (numbersList[0], numbersList[1])
             #print(numbersList)
             #print(wardrobeDict)
     
             allWardrobes[wardrobeName] = wardrobeDict
 
-# print(xref, "\n\n")
-# print(allWardrobes, "\n\n")
+#print(xref, "\n\n")
+#print(allWardrobes, "\n\n")
 
 #use dict.get to pull xref and create output 
-
-
+#loop through data structure and output 
 for wardrobe in allWardrobes :
     print(wardrobe)
     #print(allWardrobes[wardrobe])
     output = "<Ped"
     for comp in allWardrobes[wardrobe] :
+        
+        if comp == "Gender" : 
+            if allWardrobes[wardrobe][comp][0] == "Male" : 
+                pedName = "MP_M_FREEMODE_01"
+                continue
+            elif allWardrobes[wardrobe][comp][0] == "Female" : 
+                pedName = "MP_F_FREEMODE_01"
+                continue
+
         compName = str(xref.get(comp)[0])
         compValue = int(allWardrobes[wardrobe][comp][0])
         textureName = str(xref.get(comp)[1])
-
         textureValue = int(allWardrobes[wardrobe][comp][1])
 
         if compValue > 1 or textureValue > 1 : #if the comp is not default or has a nondefault texture : print comp
@@ -72,7 +87,8 @@ for wardrobe in allWardrobes :
                 output += " {0}=\"{1}\"".format(textureName, textureValue)
 
         #print(allWardrobes[wardrobe][comp])
-    output += ">INSERT PED NAME HERE</Ped>"
+
+    output += ">{}</Ped>".format(pedName)
     print(output, "\n")
 
     #### add feature to insert ped name or default to MP_F_FREEMODE_01
